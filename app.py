b2919d883924e2786d9334500f420de2e5b58048
Жанна Азарян <azaryan05@icloud.com>
2025-09-24 16:45:03 +0700

    Имя студента

--- app.py
+++ app.py
@@ -432,4 +432,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Жанна Азарян'
+    return render_template('example.html', name=name)
