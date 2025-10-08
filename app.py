3f9c72fa52718e85d2e3e231cc37a3a0231a1418
Жанна Азарян <azaryan05@icloud.com>
2025-10-08 22:19:07 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 visit_log = []
 @app.errorhandler(404)
@@ -78,6 +80,9 @@ def index():
             </li>
             <li>
             <a href='/lab2'>Лабораторная работа 2</a>
+            </li>
+            <li>
+            <a href='/lab3'>Лабораторная работа 3</a>
             </li>
         </ol>
         </main>
